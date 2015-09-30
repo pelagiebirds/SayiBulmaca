ca6124321f9ccbc6f053ff00d44622bf7aa2cbe5
pbird1 <mehmet@pelagie-birds.org>
2015-10-01 00:10:22 +0300

    Changed default language from Turkish to English
    Signed-off-by: pbird1 <mehmet@pelagie-birds.org>

--- sayi.bulmaca.js
+++ sayi.bulmaca.js
@@ -556,98 +556,98 @@ function SayiBulmaca (container, options) {
 }
 
 SayiBulmaca.prototype.locales = [];
-SayiBulmaca.prototype.locales['tr'] = {
+SayiBulmaca.prototype.locales['en'] = {
 	languageName: function () {
-		return "Türkçe";
+		return "English";
 	},
 	errorInHoldedNumber: function() {
-		return "Kurallara aykırı bir sayı tuttunuz.";
+		return "You can't pick this number. Read the help section please.";
 	},
 	errorInGuessNumber: function () {
-		return "Kurallara aykırı bir tahmin yaptınız.";
+		return "Your guess is not satisfying the number picking rules.";
 	},
 	guessButton: function() {
-		return "Tahmin";
+		return "Guess";
 	},
 	guessPlaceholder: function() {
-		return "Tahmininizi yazınız";
+		return "Write your guess here.";
 	},
 	userWinText: function () {
-		return "Tebrikler! Kazandınız";
+		return "Conguratulations! You win";
 	},
 	userWinTitle: function () {
-		return "Bitti";
+		return "Finish";
 	},
 	cpuWinText: function () {
-		return "Bilgisayar kazandı";
+		return "Computer wins";
 	},
 	cpuNumber: function () {
-		return "Bilgisayarın tuttuğu sayı:";
+		return "Computer picked:";
 	},
 	cpuWinTitle: function () {
-		return "Bitti";
+		return "Finish";
 	},
 	beginButton: function () {
-		return "Başla";
+		return "Begin";
 	},
 	holdNumberPlaceholder: function () {
-		return "Tuttuğunuz sayıyı yazınız";
+		return "Pick a number";
 	},
 	you: function () {
-		return "SİZ";
+		return "YOU";
 	},
 	cpu: function () {
-		return "BİLGİSAYAR";
+		return "COMPUTER";
 	},
 	guess: function () {
-		return "Tahmin";
+		return "Guess";
 	},
 	result: function () {
-		return "Sonuç";
+		return "Result";
 	},
 	howToTitle: function () {
-		return "Nasıl Oynanır?";
+		return "How to Play?";
 	},
 	howToList1: function () {
-		return "Rakamları birbirinden farklı 4 basamaklı bir sayı tutun. (0 ile başlayabilir)";
+		return "Pick a 4-digit number, all digits should be different from each other. (can start with 0)";
 	},
 	howToList2: function () {
-		return "Oyunu başlattıktan sonra bilgisayarın tuttuğu sayıyı bulmaya çalışın, o sizinkini bulmadan önce.";
+		return "After pressing Begin button try finding computer's number, before it finds yours.";
 	},
 	howToList3: function () {
-		return "Her tahminizden sonra bir ipucu elde edersiniz.";
+		return "By making guesses, you get clues.";
 	},
 	howToList4: function () {
-		return "+n: Tahmininizdeki n adet rakam tutulan sayıda var ve doğru basamakta demektir.";
+		return "+n: n digits are at the correct position (step) in computer's number.";
 	},
 	howToList5: function () {
-		return "-n: Tahmininizdeki n adet rakam tutulan sayıda var ve yanlış basamakta demektir.";
+		return "-n: n digits are at the wrong position (step) in computer's number.";
 	},
 	howToList6: function () {
-		return "0: Tahmininizdeki hiçbir rakam tutulan sayıda yok demektir.";
+		return "0: None of the digits exist in computer's numbers.";
 	},
 	okButton: function () {
-		return "Tamam";
+		return "OK";
 	},
 	cancelButton: function () {
-		return "İptal";
+		return "Cancel";
 	},
 	error: function () {
-		return "Hata";
+		return "Error";
 	},
 	settingsTitle: function () {
-		return "Ayarlar";
+		return "Settings";
 	},
 	excludeZeroSettingTitle: function () {
-		return "Tutulan Sayıda 0";
+		return "Picked Number";
 	},
 	excludeZeroSettingText: function () {
-		return "Olabilsin";
+		return "Can include 0";
 	},
 	languageSettingTitle: function () {
-		return "Dil";
+		return "Language";
 	},
 	languageSettingText: function () {
-		return "Dil Seçin...";
+		return "Choose...";
 	}
 }
---
+++ sayi.bulmaca.locales.tr.js
@@ -0,0 +1,95 @@
+SayiBulmaca.prototype.locales['tr'] = {
+	languageName: function () {
+		return "Türkçe";
+	},
+	errorInHoldedNumber: function() {
+		return "Kurallara aykırı bir sayı tuttunuz.";
+	},
+	errorInGuessNumber: function () {
+		return "Kurallara aykırı bir tahmin yaptınız.";
+	},
+	guessButton: function() {
+		return "Tahmin";
+	},
+	guessPlaceholder: function() {
+		return "Tahmininizi yazınız";
+	},
+	userWinText: function () {
+		return "Tebrikler! Kazandınız";
+	},
+	userWinTitle: function () {
+		return "Bitti";
+	},
+	cpuWinText: function () {
+		return "Bilgisayar kazandı";
+	},
+	cpuNumber: function () {
+		return "Bilgisayarın tuttuğu sayı:";
+	},
+	cpuWinTitle: function () {
+		return "Bitti";
+	},
+	beginButton: function () {
+		return "Başla";
+	},
+	holdNumberPlaceholder: function () {
+		return "Tuttuğunuz sayıyı yazınız";
+	},
+	you: function () {
+		return "SİZ";
+	},
+	cpu: function () {
+		return "BİLGİSAYAR";
+	},
+	guess: function () {
+		return "Tahmin";
+	},
+	result: function () {
+		return "Sonuç";
+	},
+	howToTitle: function () {
+		return "Nasıl Oynanır?";
+	},
+	howToList1: function () {
+		return "Rakamları birbirinden farklı 4 basamaklı bir sayı tutun. (0 ile başlayabilir)";
+	},
+	howToList2: function () {
+		return "Oyunu başlattıktan sonra bilgisayarın tuttuğu sayıyı bulmaya çalışın, o sizinkini bulmadan önce.";
+	},
+	howToList3: function () {
+		return "Her tahminizden sonra bir ipucu elde edersiniz.";
+	},
+	howToList4: function () {
+		return "+n: Tahmininizdeki n adet rakam tutulan sayıda var ve doğru basamakta demektir.";
+	},
+	howToList5: function () {
+		return "-n: Tahmininizdeki n adet rakam tutulan sayıda var ve yanlış basamakta demektir.";
+	},
+	howToList6: function () {
+		return "0: Tahmininizdeki hiçbir rakam tutulan sayıda yok demektir.";
+	},
+	okButton: function () {
+		return "Tamam";
+	},
+	cancelButton: function () {
+		return "İptal";
+	},
+	error: function () {
+		return "Hata";
+	},
+	settingsTitle: function () {
+		return "Ayarlar";
+	},
+	excludeZeroSettingTitle: function () {
+		return "Tutulan Sayıda 0";
+	},
+	excludeZeroSettingText: function () {
+		return "Olabilsin";
+	},
+	languageSettingTitle: function () {
+		return "Dil";
+	},
+	languageSettingText: function () {
+		return "Dil Seçin...";
+	}
+}
